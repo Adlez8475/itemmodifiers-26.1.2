dbc623f44837ea287ef70a83af3f776c6653fa69
Adlez8475 <wolfman8574@gmail.com>
2026-09-11 12:29:20 +0200

    Adding name, use ModifierService.ModifierAttribute, getName() created

--- src/main/java/net/adlez/itemmodifiers/modifiers/Modifier.java
+++ src/main/java/net/adlez/itemmodifiers/modifiers/Modifier.java
@@ -1,26 +1,66 @@
 package net.adlez.itemmodifiers.modifiers;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+
+
+import java.util.List;
 
 public enum Modifier {
-    UNCHANGED(Rarity.UNCHANGED),
-    POINTY(Rarity.COMMON),
-    RAPID(Rarity.UNCOMMON),
-    MURDEROUS(Rarity.RARE),
-    DEMONIC(Rarity.EPIC),
-    LEGENDARY(Rarity.LEGENDARY),
-    UNREAL(Rarity.MYTHIC),
+    UNCHANGED("Unchanged",Rarity.UNCHANGED),
+    POINTY(
+            "Pointy",
+            Rarity.COMMON,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_DAMAGE, 0.1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
+    RAPID(
+            "Rapid",
+            Rarity.UNCOMMON,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_SPEED, 0.15, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
+    MURDEROUS(
+            "Murderous",
+            Rarity.RARE,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_DAMAGE, 0.125F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL),
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_SPEED, 0.125F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
+    DEMONIC(
+            "Demonic",
+            Rarity.EPIC,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_DAMAGE, 0.15, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
+    LEGENDARY(
+            "Legendary",
+            Rarity.LEGENDARY,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_DAMAGE, 0.15, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL),
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_SPEED, 0.1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
+    UNREAL(
+            "Unreal",
+            Rarity.MYTHIC,
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_DAMAGE, 0.4, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL),
+            new ModifierService.ModifierAttribute(Attributes.ATTACK_SPEED, 0.25F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+    ),
     ;
 
-    private Rarity rarity;
+    private final String name;
+    private final Rarity rarity;
+    private final List<ModifierService.ModifierAttribute> attribute;
 
     public static final Codec<Modifier> MODIFIER_CODEC = Codec.STRING.xmap(Modifier::valueOf, Modifier::name);
 
-    Modifier(Rarity rarity) {
+    Modifier(String name, Rarity rarity, ModifierService.ModifierAttribute... attribute) {
+        this.name = name;
         this.rarity = rarity;
+        this.attribute = List.of(attribute);
     }
+
+
 
     public Rarity getRarity() {
         return this.rarity;
     }
+    public List<ModifierService.ModifierAttribute> getAttribute() { return this.attribute; }
+    public String getName() { return this.name; }
 }
