119bbd236f236c7920e5c321d80e854e7e86115f
Adlez8475 <wolfman8574@gmail.com>
2026-09-14 21:04:07 +0200

    Change SWORD to WEAPON in order to include Spears

--- src/main/java/net/adlez/itemmodifiers/modifiers/ItemType.java
+++ src/main/java/net/adlez/itemmodifiers/modifiers/ItemType.java
@@ -5,7 +5,7 @@ import net.minecraft.tags.ItemTags;
 
 
 public enum ItemType {
-    SWORD,
+    WEAPONS,
     BOW,
     ARMOR,
     TOOLS,
@@ -15,8 +15,8 @@ public enum ItemType {
         if (stack.is(ItemTags.FOOT_ARMOR) || stack.is(ItemTags.LEG_ARMOR) || stack.is(ItemTags.CHEST_ARMOR) || stack.is(ItemTags.HEAD_ARMOR)) {
             return ARMOR;
         }
-        if (stack.is(ItemTags.SWORDS)) {
-            return SWORD;
+        if (stack.is(ItemTags.SWORDS) || stack.is(ItemTags.SPEARS)) {
+            return WEAPONS;
         }
         if (stack.is(ItemTags.AXES) || stack.is(ItemTags.SHOVELS) || stack.is(ItemTags.HOES) || stack.is(ItemTags.PICKAXES)) {
             return TOOLS;
