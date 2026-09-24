c145820dccea6cfe61a7454f4171f0083d484292
Adlez8475 <wolfman8574@gmail.com>
2026-09-24 15:08:25 +0200

    Now supports Mace and Trident

--- src/main/java/net/adlez/itemmodifiers/modifiers/ItemType.java
+++ src/main/java/net/adlez/itemmodifiers/modifiers/ItemType.java
@@ -15,7 +15,7 @@ public enum ItemType {
         if (stack.is(ItemTags.FOOT_ARMOR) || stack.is(ItemTags.LEG_ARMOR) || stack.is(ItemTags.CHEST_ARMOR) || stack.is(ItemTags.HEAD_ARMOR)) {
             return ARMOR;
         }
-        if (stack.is(ItemTags.SWORDS) || stack.is(ItemTags.SPEARS)) {
+        if (stack.is(ItemTags.SWORDS) || stack.is(ItemTags.SPEARS) || stack.getItem() instanceof MaceItem || stack.getItem() instanceof TridentItem) {
             return WEAPONS;
         }
         if (stack.is(ItemTags.AXES) || stack.is(ItemTags.SHOVELS) || stack.is(ItemTags.HOES) || stack.is(ItemTags.PICKAXES)) {
